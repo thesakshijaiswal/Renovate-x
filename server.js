596cf2741389d6cf6ee63256827a17f28653a925
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-05 17:07:32 +0000

    use file system module to rename files

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const newVal = "Project";
 try {
   const data = fs.readdir("data", (err, data) => {
     console.log(data);
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      const newFile = item.replaceAll(oldVal, newVal);
+      fs.rename("data/" + item, newFile, () => {
+        console.log("Renamed successfully!");
+      });
+    }
   });
 } catch (err) {
   console.error(err);
