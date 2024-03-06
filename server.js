305c445e6cb4104b830401ac44d5a8de5cf6f125
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-06 12:54:10 +0000

    rename only when preview is not true

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const fs = require("node:fs");
 const oldVal = "seminar";
 const newVal = "Project";
+const preview = true;
 try {
   const data = fs.readdir("data", (err, data) => {
-    console.log(data);
     for (let index = 0; index < data.length; index++) {
       const item = data[index];
       const newFile = item.replaceAll(oldVal, newVal);
-      fs.rename("data/" + item, newFile, () => {
-        console.log("Renamed successfully!");
-      });
+      if (!preview) {
+        fs.rename("data/" + item, newFile, () => {
+          console.log("Renamed successfully!");
+        });
+      } else {
+        if ("data/" + item !== newFile) {
+          console.log("data/" + item + "will be renamed to" + newFile);
+        }
+      }
     }
   });
 } catch (err) {
